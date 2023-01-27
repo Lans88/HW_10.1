e7d11ca3186fbd5fc061b2c0da5c9de2a6b0b1dc
Artem <art001.163@gmail.com>
2023-01-27 07:39:39 +0400

    Внесение правок в ДЗ

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -39,4 +39,21 @@ public class ManagerTest {
         String[] actual = manager.findLastFilms();
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void testFindLastLimTestMax() {
+        FilmsManager manager = new FilmsManager(10);
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 5");
+        manager.addFilm("Film 6");
+        manager.addFilm("Film 7");
+        manager.addFilm("Film 8");
+        manager.addFilm("Film 9");
+        manager.addFilm("Film 10");
+        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5","Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLastFilms();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
