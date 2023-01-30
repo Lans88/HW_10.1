c746e710caade2c7ebe05f1004910833932c6c75
Artem <art001.163@gmail.com>
2023-01-30 08:10:38 +0400

    Правки ДЗ 2

--- src/main/java/ru/netology/hw10/FilmsManager.java
+++ src/main/java/ru/netology/hw10/FilmsManager.java
@@ -2,13 +2,16 @@ package ru.netology.hw10;
 
 public class FilmsManager {
     private String[] films = new String[0];
+
     public FilmsManager() {
 
     }
-    public FilmsManager(int limit){
+
+    public FilmsManager(int limit) {
 
     }
-    public void addFilm(String film){
+
+    public void addFilm(String film) {
         String[] temp = new String[films.length + 1];
         for (int i = 0; i < films.length; i += 1) {
             temp[i] = films[i];
@@ -16,18 +19,20 @@ public class FilmsManager {
         temp[temp.length - 1] = film;
         films = temp;
     }
+
     public String[] allFilms() {
         return films;
     }
-    public String[] findLastFilms() {
+
+    public String[] findLastFilms(int limit) {
         int resultLenght;
-        if (films.length < 10) {
+        if (films.length < limit) {
             resultLenght = films.length;
         } else {
             resultLenght = 10;
         }
         String[] temp = new String[resultLenght];
-        for (int i = 0; i < temp.length; i += 1){
+        for (int i = 0; i < temp.length; i += 1) {
             temp[i] = films[films.length - 1 - i];
         }
         return temp;
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -16,6 +16,7 @@ public class ManagerTest {
         String[] actual = manager.allFilms();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void testFindLast() {
         FilmsManager manager = new FilmsManager();
@@ -26,9 +27,10 @@ public class ManagerTest {
         manager.addFilm("Film 5");
 
         String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
-        String[] actual = manager.findLastFilms();
+        String[] actual = manager.findLastFilms(8);
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void testFindLastLim() {
         FilmsManager manager = new FilmsManager(7);
@@ -36,9 +38,10 @@ public class ManagerTest {
         manager.addFilm("Film 2");
         manager.addFilm("Film 3");
         String[] expected = {"Film 3", "Film 2", "Film 1"};
-        String[] actual = manager.findLastFilms();
+        String[] actual = manager.findLastFilms(7);
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void testFindLastLimTestMax() {
         FilmsManager manager = new FilmsManager(10);
@@ -52,8 +55,8 @@ public class ManagerTest {
         manager.addFilm("Film 8");
         manager.addFilm("Film 9");
         manager.addFilm("Film 10");
-        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5","Film 4", "Film 3", "Film 2", "Film 1"};
-        String[] actual = manager.findLastFilms();
+        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLastFilms(10);
         Assertions.assertArrayEquals(expected, actual);
     }
 }
